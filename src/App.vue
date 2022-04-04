2b2bcf81bff42d396ea52127b187f2bbca9548ab
kimhyera <khr1828@naver.com>
2022-04-05 01:01:48 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template lang="">
-  <div>asdfadsdf</div>
+	<div>asdfadsdf</div>
 </template>
 <script>
 export default {
-  created() {
-    console.log("dadf");
-    const ss = 0;
-  },
-  methods: {
-    test(ddd) {
-      console.log("asf");
-    },
-  },
-};
+	created() {
+		console.log('dadf')
+		const ss = 0
+	},
+	methods: {
+		test(ddd) {
+			console.log('asf')
+		},
+	},
+}
 </script>
 <style lang=""></style>
